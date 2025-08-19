6a0df0ed36b4a975d4672f827a0f35a8cdb70fd7
Victor Raul De la Torre Anicama <victor.torrea@elektra.com.mx>
2025-08-19 00:15:00 -0500

    Initial Commit

---
+++ CountriesTest/CountriesTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesTestApp.swift
+//  CountriesTest
+//
+//  Created by Victor Raul De la Torre Anicama on 19/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CountriesTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
